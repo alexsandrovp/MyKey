e5775a9371272b2643b1878dbb2ed668e5bd392b
Alex Vargas <alexsandrovp@gmail.com>
2013-12-28 12:07:38 -0200

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace MyKey
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(369, 27);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(393, 42);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "My key";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ deobfuscator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+using Microsoft.Win32;
+
+namespace MyKey
+{
+    class deobfuscator
+    {
+        public static string getSerial(string from = @"SOFTWARE\Microsoft\Windows NT\CurrentVersion", string valueName = "DigitalProductId")
+        {
+            RegistryKey hive = null;
+            RegistryKey key = null;
+            try
+            {
+                string result = "";
+                hive = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Default);
+                key = hive.OpenSubKey(from, false);
+                RegistryValueKind k = RegistryValueKind.Unknown;
+                try { k = key.GetValueKind(valueName); }
+                catch (Exception) { }
+
+                if (k == RegistryValueKind.Unknown)
+                {
+                    key.Close();
+                    hive.Close();
+                    hive = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+                    key = hive.OpenSubKey(from, false);
+                    try { k = key.GetValueKind(valueName); }
+                    catch (Exception) { }
+                }
+
+                if (k == RegistryValueKind.Binary)
+                {
+                    int pivot = 0;
+                    byte[] bytes = (byte[])key.GetValue(valueName);
+                    int[] ints = new int[16];
+                    for (int i = 52; i < 67; ++i) ints[i - 52] = bytes[i];
+                    for (int i = 0; i < 25; ++i)
+                    {
+                        pivot = 0;
+                        for (int j = 14; j >= 0; --j)
+                        {
+                            pivot <<= 8;
+                            pivot ^= ints[j];
+                            ints[j] = ((int)Math.Truncate(pivot / 24.0));
+                            pivot %= 24;
+                        }
+                        result = possible_chars[pivot] + result;
+                        if ((i % 5 == 4) && (i != 24)) result = "-" + result;
+                    }
+                }
+                return result;
+            }
+            catch (Exception e) { return e.Message; }
+            finally
+            {
+                if (null != key) key.Close();
+                if (null != hive) hive.Close();
+            }
+        }
+
+        private static readonly string possible_chars = "BCDFGHJKMPQRTVWXY2346789";
+    }
+}
